3e41ee0ee5f150e880ef5495fb3000e98f418c72
Archit jaiswal <architjaiswal@Archits-MacBook-Pro.local>
2025-03-08 14:59:04 +0530

    deployed to prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/search-box/',
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts',
     css: true
   }
-})
+});
